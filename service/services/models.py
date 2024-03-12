f5e90869999f30749f59b4c53c93e48a66490348
Dexon12 <denzuravl@mail.ru>
2024-03-12 18:07:50 +0100

    ORM query optimization first steps

--- service/services/models.py
+++ service/services/models.py
@@ -8,6 +8,8 @@ class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
 
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 class Plan(models.Model):
     PLAN_TYPES = (
@@ -19,10 +21,13 @@ class Plan(models.Model):
     plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
     discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
+    def __str__(self) -> str:
+        return f'{self.plan_type}'
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
 
-
+    def __str__(self) -> str:
+        return f'Client: {self.client} | Service: {self.service} | Plan: {self.plan}'
