b849d2a06c66a74edeacf637d67b3f4b609699b2
Dexon12 <denzuravl@mail.ru>
2024-03-12 18:34:14 +0100

    ORM queries optimazation

--- service/services/views.py
+++ service/services/views.py
@@ -8,8 +8,18 @@ from services.models import Subscription
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', 
-                queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-        )
+                queryset=Client.objects.all().select_related('user').only(
+                    'company_name', 'user__email'
+                    ))
+        ) # do 3 requests to db
+
+    # Do 1 request but with JOIN
+    # queryset = Subscription.objects.all().select_related('plan', 'client', 'client__user').only(
+    #     'client__user__email',
+    #     'client__company_name',
+    #     'plan_id',
+    # )
     serializer_class = SubscriptionSerializer
     
